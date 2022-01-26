aabcab8d228004ab9ccf2b8b2dad5c000498edb4
Hamleto Muradyan <hamomuradyan93@gmail.com>
2022-01-26 23:02:16 +0400

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const WINNING_SUM = 123.40;
 /**
 @param {Array} cartItemsPrices
 */
+
 function doesCartWinPrize(cartItemsPrices) {
-  
+    const result = cartItemsPrices.reduce((acc, item) => {
+        return Number(acc) + Number(item);
+    })
+    if(Math.round(result * 100) / 100 === WINNING_SUM){
+        return true;
+    }
+    return false;
 }
 
 module.exports = doesCartWinPrize;
